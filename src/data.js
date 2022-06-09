0ce09fea4491ac15d85458092149105519de56d4
Natanniel <natanniel.alves@outlook.com>
2022-06-08 21:41:10 -0300

    deplot

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ import axios from "axios";
 //import Cookies from "js-cookie";
 
 //var token = Cookies.get("apollo");
-var host = "http://localhost:3000";
+var host = "http://143.198.244.180:3000";
 
 const api = axios.create({
   baseURL: host,
